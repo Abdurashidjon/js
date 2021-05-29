f4adfd447d4ff9b1a0f5c89b245b05edb32240c4
Abdurashidjon <abdurashidgaybullayev@gmail.com>
2021-05-29 09:22:14 +0500

    My js new works

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-console.log("assalomu alaykum")
+//2 xil ozgaruvchilar haqida gaplashamiz bu darsda 1- Primitiv yoki value turlari 2- reference 
+// Primitiv bilan tanishamiz bularga  String,Number,Boolean,undefined,null kiradi  
+
+
+let name='Abdurashid'; //string
+console.log(typeof(name));
+let age=22; //number
+console.log(typeof(age));
+let isMarried=false; //boolean
+console.log(typeof(isMarried));
+let firstName=undefined; //undefined
+console.log(typeof(firstName));
+let selectedColor=null; //null
+selectedColor='black';
+selectedColor=null;
+console.log(typeof(selectedColor));
