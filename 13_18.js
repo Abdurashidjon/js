c50541d5ab6ba0a3aa65a6c0bbf72154b5e8839d
Abdurashidjon <abdurashidgaybullayev@gmail.com>
2021-05-31 15:42:48 +0500

    My workss'

--- 13_18.js
+++ 13_18.js
@@ -1,3 +1,49 @@
 // Demak bugungi darsdan boshlab if va else ni bowlaymiz
+/* if (shart) {
+    statement1;
+    statement2;
+}else if (bowqaShart) {
+    statement3;
+}else{
+    statement4;
+} */
+//13
+/* let hour=+prompt('Soatni kiriting:');
+
+if (hour>=6 && hour<12) {
+    console.log("Xayrli tong");
+}else if (hour>=12 && hour<18) {
+    console.log("Xayrli kun");
+} else if (hour>=18 && hour<22) {
+    console.log("Xayrli kech");
+} else{
+console.log("Xayrli tun");
+} */
+
+//14 switch case
+/* let word=prompt('Biror suz kiriting tarjima qiberamiz');
+    switch (word) {
+        case 'car':
+            console.log("Avtomashina");
+            break;
+
+        case 'book':
+            console.log("Kitob");
+            break;
+
+            case 'bcycle':
+                console.log("Velosiped");
+                break;
+
+        default:
+            console.log("Bizning lugatda bunaqa so'z yo'q: ")
+    } */ // biz buni if elseda qilsak ham boladi dasturchi oziga qulay tomonini qilsa bolaveradi
 
 
+
+    //15 loop lar haqida taniwamiz. for loop ni koramiz
+    for (let i = 1; i <=5; i++){
+        if (i%2==1) {
+            console.log('salom '+i)
+        } 
+    }
